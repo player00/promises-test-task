592407aee3c9ce0f8c9f7c45d1fc4c2a4b89ebfe
shalygins <shalygins23@gmail.com>
2023-07-05 18:39:00 +0300

    reordered tests

--- tests/task.test.js
+++ tests/task.test.js
@@ -7,21 +7,10 @@ const {
 } = require('../constants.js');
 
 describe('resolvedWithRejectionNumCheck', () => {
-	it('should give proper message for empty array', () => {
-		const promises = []
-		const maxRejectedNum = 1
-		const expectedOutput = NO_PROMISES
-
-		const output = resolvedWithRejectionNumCheck(promises, maxRejectedNum)
-
-		expect(output).rejects.toEqual(expectedOutput)
-	})
-
-	it('should resolve when amount of rejected promises equals maxRejectedNum', () => {
+	it('should resolve when all promises are resolved', () => {
 		const resolvedPromise = Promise.resolve('success')
-		const rejectedPromise = Promise.reject('fail')
-		const promises = [resolvedPromise, resolvedPromise, rejectedPromise]
-		const maxRejectedNum = 1
+		const promises = [resolvedPromise, resolvedPromise, resolvedPromise]
+		const maxRejectedNum = 2
 		const expectedOutput = SUCCESS
 
 		const output = resolvedWithRejectionNumCheck(promises, maxRejectedNum)
@@ -41,12 +30,34 @@ describe('resolvedWithRejectionNumCheck', () => {
 		expect(output).resolves.toEqual(expectedOutput)
 	})
 
+	it('should resolve when amount of rejected promises equals maxRejectedNum', () => {
+		const resolvedPromise = Promise.resolve('success')
+		const rejectedPromise = Promise.reject('fail')
+		const promises = [resolvedPromise, resolvedPromise, rejectedPromise]
+		const maxRejectedNum = 1
+		const expectedOutput = SUCCESS
+
+		const output = resolvedWithRejectionNumCheck(promises, maxRejectedNum)
+
+		expect(output).resolves.toEqual(expectedOutput)
+	})
+
 	it('should reject when amount of rejected promises exceeds maxRejectedNum', () => {
 		const resolvedPromise = Promise.resolve('success')
 		const rejectedPromise = Promise.reject('fail')
 		const promises = [resolvedPromise, rejectedPromise, rejectedPromise]
 		const maxRejectedNum = 1
 		const expectedOutput = EXCEEDS_MAX_REJECTED_NUM
+
+		const output = resolvedWithRejectionNumCheck(promises, maxRejectedNum)
+
+		expect(output).rejects.toEqual(expectedOutput)
+	})
+
+	it('should reject for empty array of promises', () => {
+		const promises = []
+		const maxRejectedNum = 1
+		const expectedOutput = NO_PROMISES
 
 		const output = resolvedWithRejectionNumCheck(promises, maxRejectedNum)
 
