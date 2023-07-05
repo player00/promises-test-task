bb06885bee6f6f91608f5fa10abded6d82dd4eae
shalygins <shalygins23@gmail.com>
2023-07-05 18:11:22 +0300

    reordered if checks

--- src/task.js
+++ src/task.js
@@ -1,10 +1,11 @@
 function resolvedWithRejectionNumCheck(promisesToHandle, maxRejectedNum) {
 	return new Promise((resolve, reject) => {
-		if (Array.isArray(promisesToHandle) === false || promisesToHandle.every(item => item instanceof Promise) === false) {
-			reject('Input must be an array of Promises');
-		}
 		if (promisesToHandle.length == 0) {
 			resolve('There was no promises');
+		}
+
+		if (Array.isArray(promisesToHandle) === false || promisesToHandle.every(item => item instanceof Promise) === false) {
+			reject('Input must be an array of Promises');
 		}
 		Promise.allSettled(promisesToHandle)
 			.then(results => results.filter(result => result.status === "rejected"))
